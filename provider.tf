99130b6b6f0ed67a3daf9f141b50d4059838e7cc
Harsh007-devops <hupadhyay1122@gmail.com>
2026-02-21 16:03:48 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.AWSaccess
+  secret_key = var.AWSsecret
   region     = var.aws_region
 }
